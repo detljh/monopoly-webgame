c01162fb2cb1eedd3e7152db72a89f58d8d3bcea
lisa <lisa.ng8@hotmail.com>
2020-03-18 16:36:22 +1100

    add visual cue for disabled buttons

--- src/app/game/GameComponent.js
+++ src/app/game/GameComponent.js
@@ -15,8 +15,8 @@ class GameComponent extends React.Component {
             <div id="game-page" className="transition-item">    
                 <div id="game-header">
                     <button onClick={this.props.newGame} className="main-buttons">New Game</button>
-                    <button onClick={this.props.endGame} className="main-buttons" disabled={disabled}>End Game</button>
-                    <button className="main-buttons " id="give-up-button" onClick={this.props.giveUp} disabled={disabled}>Give Up</button>
+                    <button onClick={this.props.endGame} className={`main-buttons disabled-${disabled}`} disabled={disabled}>End Game</button>
+                    <button className={`main-buttons disabled-${disabled}`} id="give-up-button" onClick={this.props.giveUp} disabled={disabled}>Give Up</button>
                     <div className="player-info-bar">
                         {this.props.players.map((element) => 
                             <PlayerInfo key={element.id} player={element} currentPlayer={element.id == this.props.currentPlayer.id ? true : false} money={element.money}/>
